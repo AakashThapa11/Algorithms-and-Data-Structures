9e6bb4c162dc688d2e7e2e850ecb6a1c0fab2096
Aakash Thapa <63937904+AakashThapa11@users.noreply.github.com>
2024-01-19 08:16:23 +1100

    Solved capitalize-first.js

--- src/recursion/capitalize-first.js
+++ src/recursion/capitalize-first.js
@@ -3,7 +3,15 @@
 // Given an array of strings, capitalize the first letter of each string in the array.
 
 function capitalizeFirst(arr) {
-  return;
+  const result = [];
+
+  if(!arr.length) return result;
+
+  const capWord = arr[0][0].toUpperCase() + arr[0].slice(1);
+
+  result.push(capWord);
+
+  return result.concat(capitalizeFirst(arr.slice(1)))
 }
 
 console.log(capitalizeFirst(["car", "taco", "banana"])); // ['Car','Taco','Banana']
