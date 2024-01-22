3b0c7e9ab7231a515b1cf2170c64f1c56f2083b8
Aakash Thapa <aakash.thapa19168@gmail.com>
2024-01-23 09:29:27 +1100

    Solved capitalize Words

--- src/recursion/capitalize-first.js
+++ src/recursion/capitalize-first.js
@@ -5,13 +5,13 @@
 function capitalizeFirst(arr) {
   const result = [];
 
-  if(!arr.length) return result;
+  if (!arr.length) return result;
 
   const capWord = arr[0][0].toUpperCase() + arr[0].slice(1);
 
   result.push(capWord);
 
-  return result.concat(capitalizeFirst(arr.slice(1)))
+  return result.concat(capitalizeFirst(arr.slice(1)));
 }
 
 console.log(capitalizeFirst(["car", "taco", "banana"])); // ['Car','Taco','Banana']
--- src/recursion/capitalize-words.js
+++ src/recursion/capitalize-words.js
@@ -3,7 +3,14 @@
 // Given an array of words, return a new array containing each word capitalized.
 
 function capitalizeWords(arr) {
-  return;
+  let result = [];
+
+  if (!arr.length) return result;
+
+  const capWord = arr[0].toUpperCase();
+
+  result.push(capWord);
+  return result.concat(capitalizeWords(arr.slice(1)));
 }
 
 const words = ["i", "am", "learning", "recursion"];
